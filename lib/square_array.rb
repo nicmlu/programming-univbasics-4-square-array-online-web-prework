1b5e2451db10e7ae1a83f49d3aaa06945dc71bd4
Nicole Lucien <nicolemlucien@gmail.com>
2019-09-25 16:12:19 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,11 @@
  def square_array(array)
+  new_array = []
+  counter = 0
   
+  while counter < array.length do 
+    new_array << array[counter] ** 2
+    counter += 1
+  end 
+
+  p new_array 
 end
